01572d572e93500a2ef6484aa70ad253e5f7d2f3
TanRandel <randeljoshuatan@gmail.com>
2023-11-18 16:49:02 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
+import Hero from "./Hero";
 import './App.css';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+      <Route path='/*' element = {<Hero/>}/>
+      </Routes>
+    </Router>
+    
+  )
 }
 
-export default App;
+export default App
+
